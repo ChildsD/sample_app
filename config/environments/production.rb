d83d69343fb13eb84bd19e400d6f8d6058d597dc
Daniel Childs <ChildsD@gmail.com>
2012-03-23 16:06:15 -0500

    Changed serve_static_assets to true in production.rb to troublehoot heroku (it says We're sorry, but something went wrong.)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ SampleApp::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # THIS WAS SET TO "false" BUT A SOLUTION ON STACK OVERFLOW SAID TO CHANGE IT TO TRUE...(3/23)
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
